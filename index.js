13fafdb79047797c7b12527505ca7ef33e85346c
miao0007 <miao188@hotmail.com>
2020-07-17 18:42:22 +0930

    pseudocode steps

---
+++ index.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+// title
+// description
+// installation instruction(installation)
+// usage information(usage)
+// contribution guidelines(contributing)
+// test instructions(tests)
+
+// this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+
+// license from a list of options
+
+// a badge for that license is added hear the top of the README
+// a notice is added to the section of the README entitled License that explains which license the application is covered
+
+// enter my GitHub username which is added to section of README entitled Questions, with a link to Github profile
+
+
+// enter email address which is added to section of README entitled Questions
+
+// click on the links then was taken to corresponding section of the README
+
+
